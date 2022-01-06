adffb3856a5a3989883869777e86f269229262a9
Mansi Manohar Mandhare <mansimandhare.mm@gmail.com>
2022-01-06 23:02:42 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -11,17 +11,17 @@ insert into Student(Name ,Roll_no , Address , Mobile_Number , PAN_Number  )
 values('abc',11,'Pune',1234567899,'SWE1234567890');
 
 
-insert into Student(Name ,Roll_no , Address , Mobile_Number , PAN_Number  )
+insert into studentData(Name ,Roll_no , Address , Mobile_Number , PAN_Number  )
 values('def',12,'Pune',3334567899,'EWQ1234567890');
 
 
-insert into Student(Name ,Roll_no , Address , Mobile_Number , PAN_Number  )
+insert into studentData(Name ,Roll_no , Address , Mobile_Number , PAN_Number  )
 values('ghi',13,'Nashik',1236787899,'OPK1234567890');
 
 
-insert into Student(Name ,Roll_no , Address , Mobile_Number , PAN_Number  )
+insert into studentData(Name ,Roll_no , Address , Mobile_Number , PAN_Number  )
 values('jkl',14,'Pune',8974567899,'DFG1234567890');
 
 
-insert into Student(Name ,Roll_no , Address , Mobile_Number , PAN_Number  )
+insert into studentData(Name ,Roll_no , Address , Mobile_Number , PAN_Number  )
 values('mno',15,'Thane',7573968899,'MNB1234567890');
